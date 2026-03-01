849ada7b30a9eddb9adb86d59a2bd91ace16ef57
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2026-03-01 18:51:41 -0300

    feat: add useAuthorById and usePostById hooks for fetching individual author and post data

--- src/hooks/useAuthors.ts
+++ src/hooks/useAuthors.ts
@@ -3,3 +3,7 @@ import { authorService } from '../api/services/author.service';
 export function useAuthors() {
   return authorService.useGetAuthors();
 }
+
+export function useAuthorById(id: string | undefined) {
+  return authorService.useGetAuthorById(id);
+}
