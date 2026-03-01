7bc09335f7f7b10e10b49e15a5ab9b8d43ef8f8e
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2026-03-01 09:02:41 -0300

    feat: integrate React Query for data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { router } from './routes';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './api/queryClient';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      {/* Develop only */}
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+    </QueryClientProvider>
   );
 }
 
